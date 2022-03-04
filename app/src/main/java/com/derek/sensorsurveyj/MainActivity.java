6c9744fe419e864f923b5842a522f425cf66a2a5
Derek <derekwellis@outlook.com>
2022-03-04 11:49:31 -0600

    Initial commit

---
+++ app/src/main/java/com/derek/sensorsurveyj/MainActivity.java
@@ -0,0 +1,36 @@
+package com.derek.sensorsurveyj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder sensorText = new StringBuilder();
+
+        for(Sensor currentSensor : sensorList) {
+            sensorText.append(currentSensor.getName()).append(System.getProperty("line.separator"));
+        }
+
+        TextView tvSensor = (TextView) findViewById(R.id.sensor_list);
+        tvSensor.setText(sensorText);
+    }
+}
